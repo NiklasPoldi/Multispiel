0b13165e07eca9da652c65c8334a5aff563c0058
Paulee-Dotcom <paulk98@gmx.de>
2019-06-12 23:09:18 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle
    #	app/build/generated/res/pngs/debug/drawable-anydpi-v21/ic_launcher_background.xml
    #	app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/android/arch/lifecycle/extensions/R$layout.class
    #	app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/android/support/asynclayoutinflater/R$layout.class
    #	app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/android/support/compat/R$layout.class
    #	app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/android/support/coordinatorlayout/R$layout.class
    #	app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/android/support/coreui/R$layout.class
    #	app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/android/support/coreutils/R$layout.class
    #	app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/android/support/customview/R$layout.class
    #	app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/android/support/design/R$layout.class
    #	app/build/outputs/apk/debug/app-debug.apk
    #	app/build/outputs/logs/manifest-merger-debug-report.txt
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/poldi/multispiel/LoginActivity.java
    #	app/src/main/java/com/poldi/multispiel/MainActivity.java
    #	app/src/main/java/com/poldi/multispiel/ScrollingActivity.java
    #	app/src/main/java/com/poldi/multispiel/data/login/LoginHandler.java
    #	app/src/main/java/com/poldi/multispiel/data/login/LoginUtils.java
    #	app/src/main/java/com/poldi/multispiel/data/ui/main/PageViewModel.java
    #	app/src/main/java/com/poldi/multispiel/data/ui/main/PlaceholderFragment.java
    #	app/src/main/java/com/poldi/multispiel/data/ui/main/SectionsPagerAdapter.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml

---
+++ app/src/main/java/com/poldi/multispiel/data/ui/main/ui/gamefragmentmodel/GameFragmentModelViewModel.java
@@ -0,0 +1,7 @@
+package com.poldi.multispiel.data.ui.main.ui.gamefragmentmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+public class GameFragmentModelViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
